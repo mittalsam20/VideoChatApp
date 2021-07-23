791df4860bc3c3bc937334b289a092b5a0f23f47
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-07-24 04:37:25 +0530

    appbar comp added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <div>
+        <AppBar position="static" color="inherit">
+          <Toolbar>
+            <Typography variant="h2" align="center">
+              Vc Meet
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        {/* Video player */}
+        {/* options -> notifications */}
+      </div>
+    </>
+  );
 }
 
 export default App;
