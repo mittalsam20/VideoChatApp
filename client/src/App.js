c1e235c199b1893a6c4c58371cb8936851837cfa
SAMAKSH MITTAL <sam.mittal44@gmail.com>
2021-07-24 11:50:23 +0530

    little bit of styling done

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,43 @@ import Options from "./components/options";
 import Notify from "./components/notify";
 import { makeStyles } from "@material-ui/styles";
 
-const useStyles = makeStyles((theme) => ({}));
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: "30px 100px",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+    width: "600px",
+    border: "2px solid black",
+
+    // [theme.breakpoints.down("xs")]: {
+    //   width: "90%",
+    // },
+  },
+  image: {
+    marginLeft: "15px",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+  },
+}));
 
 const App = () => {
   const classes = useStyles();
   return (
     <>
       <div className={classes.wrapper}>
-        <AppBar position="static" color="inherit" className={classes.appbar}>
-          <Toolbar>
-            <Typography variant="h3" align="center">
-              Vc Meet
-            </Typography>
-          </Toolbar>
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography variant="h3" align="center">
+            Vc Meet
+          </Typography>
         </AppBar>
+
         <Vplayer />
         <Options>
           <Notify />
